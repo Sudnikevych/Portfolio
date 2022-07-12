943f8cca149522d5c94f335a4fcbe4b98bf55e9e
Sudnikevych <sudnikevych@gmail.com>
2022-07-12 23:41:42 +0300

    +about, +gallery

---
+++ js/slider-center.js
@@ -0,0 +1,9 @@
+$(".slider-center-list").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: ".slider-center-btn-prev",
+  nextArrow: ".slider-center-btn-next",
+});
